97be442aa17e937bf9a4fe08779871bf638f5e85
BahadirAydin <bahadiraydin1923@gmail.com>
2024-01-03 03:55:08 +0300

    managed to display a simple square..

--- main.cpp
+++ main.cpp
@@ -347,84 +347,56 @@ void initShaders()
 
 void initVBO()
 {
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	assert(vao > 0);
-	glBindVertexArray(vao);
-	cout << "vao = " << vao << endl;
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    assert(vao > 0);
+    glBindVertexArray(vao);
+    cout << "vao = " << vao << endl;
 
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	assert(glGetError() == GL_NONE);
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    assert(glGetError() == GL_NONE);
 
-	glGenBuffers(1, &gVertexAttribBuffer);
-	glGenBuffers(1, &gIndexBuffer);
+    glGenBuffers(1, &gVertexAttribBuffer);
+    glGenBuffers(1, &gIndexBuffer);
 
-	assert(gVertexAttribBuffer > 0 && gIndexBuffer > 0);
+    assert(gVertexAttribBuffer > 0 && gIndexBuffer > 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, gVertexAttribBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, gVertexAttribBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIndexBuffer);
 
-	gVertexDataSizeInBytes = gVertices.size() * 3 * sizeof(GLfloat);
-	gNormalDataSizeInBytes = gNormals.size() * 3 * sizeof(GLfloat);
-	int indexDataSizeInBytes = gFaces.size() * 3 * sizeof(GLuint);
-	GLfloat* vertexData = new GLfloat[gVertices.size() * 3];
-	GLfloat* normalData = new GLfloat[gNormals.size() * 3];
-	GLuint* indexData = new GLuint[gFaces.size() * 3];
+    gVertexDataSizeInBytes = 4 * 3 * sizeof(GLfloat);  // 4 vertices for a rectangle
+    gNormalDataSizeInBytes = 4 * 3 * sizeof(GLfloat);  // 4 normals for a rectangle
+    int indexDataSizeInBytes = 2 * 3 * sizeof(GLuint); // 2 triangles for a rectangle
 
-	float minX = 1e6, maxX = -1e6;
-	float minY = 1e6, maxY = -1e6;
-	float minZ = 1e6, maxZ = -1e6;
+    GLfloat vertexData[] = {
+        -0.5f, -0.5f, 0.0f, // Vertex 1
+         0.5f, -0.5f, 0.0f, // Vertex 2
+        -0.5f,  0.5f, 0.0f, // Vertex 3
+         0.5f,  0.5f, 0.0f  // Vertex 4
+    };
 
-	for (int i = 0; i < gVertices.size(); ++i)
-	{
-		vertexData[3 * i] = gVertices[i].x;
-		vertexData[3 * i + 1] = gVertices[i].y;
-		vertexData[3 * i + 2] = gVertices[i].z;
+    GLfloat normalData[] = {
+        0.0f, 0.0f, 1.0f, // Normal for Vertex 1
+        0.0f, 0.0f, 1.0f, // Normal for Vertex 2
+        0.0f, 0.0f, 1.0f, // Normal for Vertex 3
+        0.0f, 0.0f, 1.0f  // Normal for Vertex 4
+    };
 
-		minX = std::min(minX, gVertices[i].x);
-		maxX = std::max(maxX, gVertices[i].x);
-		minY = std::min(minY, gVertices[i].y);
-		maxY = std::max(maxY, gVertices[i].y);
-		minZ = std::min(minZ, gVertices[i].z);
-		maxZ = std::max(maxZ, gVertices[i].z);
-	}
+    GLuint indexData[] = {
+        0, 1, 2, // Triangle 1
+        1, 3, 2  // Triangle 2
+    };
 
-	std::cout << "minX = " << minX << std::endl;
-	std::cout << "maxX = " << maxX << std::endl;
-	std::cout << "minY = " << minY << std::endl;
-	std::cout << "maxY = " << maxY << std::endl;
-	std::cout << "minZ = " << minZ << std::endl;
-	std::cout << "maxZ = " << maxZ << std::endl;
+    glBufferData(GL_ARRAY_BUFFER, gVertexDataSizeInBytes + gNormalDataSizeInBytes, 0, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, gVertexDataSizeInBytes, vertexData);
+    glBufferSubData(GL_ARRAY_BUFFER, gVertexDataSizeInBytes, gNormalDataSizeInBytes, normalData);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexDataSizeInBytes, indexData, GL_STATIC_DRAW);
 
-	for (int i = 0; i < gNormals.size(); ++i)
-	{
-		normalData[3 * i] = gNormals[i].x;
-		normalData[3 * i + 1] = gNormals[i].y;
-		normalData[3 * i + 2] = gNormals[i].z;
-	}
-
-	for (int i = 0; i < gFaces.size(); ++i)
-	{
-		indexData[3 * i] = gFaces[i].vIndex[0];
-		indexData[3 * i + 1] = gFaces[i].vIndex[1];
-		indexData[3 * i + 2] = gFaces[i].vIndex[2];
-	}
-
-
-	glBufferData(GL_ARRAY_BUFFER, gVertexDataSizeInBytes + gNormalDataSizeInBytes, 0, GL_STATIC_DRAW);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, gVertexDataSizeInBytes, vertexData);
-	glBufferSubData(GL_ARRAY_BUFFER, gVertexDataSizeInBytes, gNormalDataSizeInBytes, normalData);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexDataSizeInBytes, indexData, GL_STATIC_DRAW);
-
-	// done copying to GPU memory; can free now from CPU memory
-	delete[] vertexData;
-	delete[] normalData;
-	delete[] indexData;
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(gVertexDataSizeInBytes));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(gVertexDataSizeInBytes));
 }
+
 
 void init()
 {
@@ -471,14 +443,27 @@ void display()
 	//modelingMatrix = glm::rotate<float>(modelingMatrix, (-180. / 180.) * M_PI, glm::vec3(0.0, 1.0, 0.0));
 
 	// Set the active program and the values of its uniform variables
+    activeProgramIndex = 1;
 	glUseProgram(gProgram[activeProgramIndex]);
 	glUniformMatrix4fv(projectionMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(projectionMatrix));
 	glUniformMatrix4fv(viewingMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(viewingMatrix));
 	glUniformMatrix4fv(modelingMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(modelingMatrix));
 	glUniform3fv(eyePosLoc[activeProgramIndex], 1, glm::value_ptr(eyePos));
-
-	// Draw the scene
 	drawModel();
+
+	matT = glm::translate(glm::mat4(1.0), glm::vec3(5.f, 0.f, -5.f));
+	matS = glm::scale(glm::mat4(1.0), glm::vec3(0.25, 0.25, 0.25));
+	matR = glm::rotate<float>(glm::mat4(1.0), (-180. / 180.) * M_PI, glm::vec3(0.0, 1.0, 0.0));
+	matRz = glm::rotate(glm::mat4(1.0), angleRad, glm::vec3(0.0, 0.0, 1.0));
+    modelingMatrix = matT  * matS * matRz * matR;
+    activeProgramIndex = 1;
+	glUseProgram(gProgram[activeProgramIndex]);
+	glUniformMatrix4fv(projectionMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+	glUniformMatrix4fv(viewingMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(viewingMatrix));
+	glUniformMatrix4fv(modelingMatrixLoc[activeProgramIndex], 1, GL_FALSE, glm::value_ptr(modelingMatrix));
+	glUniform3fv(eyePosLoc[activeProgramIndex], 1, glm::value_ptr(eyePos));
+	drawModel();
+
 
 	angle += 0.9;
 }
